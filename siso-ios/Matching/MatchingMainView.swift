239ec480f3d47c63545901aedc77f4e18942fc77
jdios <jdios@jdiosui-MacBookAir.local>
2025-08-11 23:28:12 +0900

    mock 진행중

--- siso-ios/Matching/MatchingMainView.swift
+++ siso-ios/Matching/MatchingMainView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct MatchingMainView: View {
+    let cardviewArray: [MatchingCardView] = Array(repeating: .init(), count: 10)
     var body: some View {
         VStack {
-            
+            ForEach(cardviewArray) { array in
+                array()
+            }
         }
     }
 }
 
-//#Preview {
-//    MatchingMainView()
-//}
+
+struct MatchingCardView: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.gray)
+                .ignoresSafeArea()
+            
+            HStack {
+                VStack {
+                    Image(systemName: "waveform")
+                    Text("김갑돌")
+                        .font(.largeTitle)
+                    Text("서울 중구")
+                        .font(.callout)
+                    Text("#운동 #투자")
+                        .font(.caption)
+                }
+                Spacer()
+                
+                Image(systemName: "phone.circle.fill")
+                    
+            }
+            .padding()
+            
+        }
+    }
+}
