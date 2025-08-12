2c6dc5887902840e55425d95a000704e84542754
cty7574 <vicpool@naver.com>
2025-08-12 11:34:26 +0900

    프로필 헤더뷰 색상 수정

--- siso-ios/Profile/View/ProfileHeaderView.swift
+++ siso-ios/Profile/View/ProfileHeaderView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ProfileHeaderView: View {
     let page: Int
     let mainColor: Color = Color(red: 1.0, green: 0.843, blue: 0.0).opacity(0.4)
-    let strokeColor: Color = Color(red: 1.0, green: 0.843, blue: 0.0).opacity(0.8)
+    let textColor: Color = .gray.opacity(0.5)
     let gray: Color = .gray.opacity(0.3)
     
     var body: some View {
@@ -21,10 +21,10 @@ struct ProfileHeaderView: View {
         HStack {
             Circle()
                 .fill(page == 1 ? mainColor : gray)
-                .stroke(strokeColor)
                 .frame(width: 36)
                 .overlay {
                     Text("1")
+                        .foregroundStyle(page == 1 ? .black : textColor)
                 }
                 
             RoundedRectangle(cornerRadius: 12)
@@ -33,10 +33,10 @@ struct ProfileHeaderView: View {
             
             Circle()
                 .fill(page == 2 ? mainColor : gray)
-                .stroke(strokeColor)
                 .frame(width: 36)
                 .overlay {
                     Text("2")
+                        .foregroundStyle(page == 2 ? .black : textColor)
                 }
             
             RoundedRectangle(cornerRadius: 12)
@@ -45,10 +45,10 @@ struct ProfileHeaderView: View {
             
             Circle()
                 .fill(page == 3 ? mainColor : gray)
-                .stroke(strokeColor)
                 .frame(width: 36)
                 .overlay {
                     Text("3")
+                        .foregroundStyle(page == 3 ? .black : textColor)
                 }
             
             RoundedRectangle(cornerRadius: 12)
@@ -57,10 +57,10 @@ struct ProfileHeaderView: View {
             
             Circle()
                 .fill(page == 4 ? mainColor : gray)
-                .stroke(strokeColor)
                 .frame(width: 36)
                 .overlay {
                     Text("4")
+                        .foregroundStyle(page == 4 ? .black : textColor)
                 }
         }
         .padding()
