29f95be9cdabfe992f8ea3fb2eac7acef9fce27b
김용해 <yoho1019@gmail.com>
2025-08-13 11:43:10 +0900

    Style: Add initial assets and AcceptanceView for Login Folder

---
+++ siso-ios/Auth/View/AcceptanceView.swift
@@ -0,0 +1,18 @@
+//
+//  AcceptanceView.swift
+//  auth
+//
+//  Created by 김용해 on 8/12/25.
+//
+
+import SwiftUI
+
+struct AcceptanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AcceptanceView()
+}
