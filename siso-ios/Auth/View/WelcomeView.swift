1e916de70da8c7dcf6c54b9f6a095bd49db5c895
김용해 <yoho1019@gmail.com>
2025-08-13 14:12:57 +0900

    Style: WelcomeView 및 관련 이미지 자원 추가

---
+++ siso-ios/Auth/View/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  auth
+//
+//  Created by 김용해 on 8/13/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
