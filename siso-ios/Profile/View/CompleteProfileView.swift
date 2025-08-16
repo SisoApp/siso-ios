3d1c3db5d92fa600810e18b22362272c8be65f50
cty7574 <vicpool@naver.com>
2025-08-16 15:06:06 +0900

    프로필 완료 뷰 UI 구성

--- siso-ios/Profile/View/CompleteProfileView.swift
+++ siso-ios/Profile/View/CompleteProfileView.swift
@@ -15,10 +15,67 @@ public struct CompleteProfileView: View {
     }
     
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("bgProfile")
+                .resizable()
+                .scaledToFill()
+            
+            VStack {
+                Spacer()
+                
+                Text("내 정보 입력을 완료하였습니다!\n시팅에서 나와 취향이 같은\n인연을 더 잘 만날 수 있어요")
+                    .font(.system(size: 24))
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                completeButton()
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .navigationTitle("내 정보 입력")
+        .navigationBarBackButtonHidden()
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Image(systemName: "chevron.backward")
+                    .foregroundStyle(Color.Siso.Gray._90)
+                    .onTapGesture {
+                        delegate?.pop()
+                    }
+            }
+        }
+    }
+    
+    private func completeButton() -> some View {
+        return Button {
+            delegate?.changeProfileToMatching()
+        } label: {
+            Text("인연 만나기")
+                .frame(maxWidth: .infinity, maxHeight: 54)
+                .font(.system(size: 18))
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+                .background(Color.Siso.Primary.main)
+                .clipShape(.rect(cornerRadius: 27))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 27)
+                        .stroke(Color.Siso.Primary._80,
+                            lineWidth: 1
+                        )
+                }
+        }
     }
 }
 
 #Preview {
-    CompleteProfileView(delegate: nil)
+    NavigationStack {
+        CompleteProfileView(delegate: nil)
+    }
 }
