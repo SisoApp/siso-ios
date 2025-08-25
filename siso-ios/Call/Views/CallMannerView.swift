de9e17cc4eebd0e7ff26936adc9f86d992d62e7b
jdios <jdios@jdiosui-MacBookAir.local>
2025-08-25 09:29:50 +0900

    cardview design update

---
+++ siso-ios/Call/Views/CallMannerView.swift
@@ -0,0 +1,18 @@
+//
+//  CallMannerView.swift
+//  call
+//
+//  Created by jdios on 8/25/25.
+//
+
+import SwiftUI
+
+struct CallMannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CallMannerView()
+}
