df5bdc4002acdc28a404f337769ce79b300e5df0
cty7574 <vicpool@naver.com>
2025-08-27 10:36:25 +0900

    관심사, 모임 태그에서 이모지 제거

--- siso-ios/Profile/ViewModel/InterestProfileViewModel.swift
+++ siso-ios/Profile/ViewModel/InterestProfileViewModel.swift
@@ -14,9 +14,9 @@ enum InterestType: String, Identifiable, CaseIterable {
 }
 
 class InterestProfileViewModel {
-    private let cultures: [String] = ["#음악감상🎧", "#사진촬영📸", "#서예🖌️", "#글쓰기✍️", "#악기연주🎸", "#영화감상🎬", "#전시관람🖼️", "#클래식감상🎻", "#노래부르기🎤", "#댄스💃🕺"]
-    private let exercises: [String] = ["#등산⛰️", "#낚시🎣", "#요가🧘‍♀️", "#골프⛳️", "#자전거🚴‍♀️", "#캠핑🏕️", "#수영🏊‍♂️", "#바둑♟️", "#볼링🎳", "#탁구🏓", "#꽃꽂이💐", "#드라이브🚗"]
-    private let hobbies: [String] = ["#독서📚", "#베이킹🧁", "#뜨개질🧶", "#원예🌿", "#여행✈️","#맛집🍽️", "#명상🧘", "#와인🍷", "#요리🍳", "#인테리어🪟"]
+    private let cultures: [String] = ["#음악감상", "#사진촬영", "#서예", "#글쓰기", "#악기연주", "#영화감상", "#전시관람", "#클래식감상", "#노래부르기", "#댄스"]
+    private let exercises: [String] = ["#등산", "#낚시", "#요가", "#골프", "#자전거", "#캠핑", "#수영", "#바둑", "#볼링", "#탁구", "#꽃꽂이", "#드라이브"]
+    private let hobbies: [String] = ["#독서", "#베이킹", "#뜨개질", "#원예", "#여행","#맛집", "#명상", "#와인", "#요리", "#인테리어"]
     
     func getInterests(_ type: InterestType) -> [String] {
         switch type {
